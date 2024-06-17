7d9097a7739288215822bfa8fe22653f6981a2b9
Yash Talele <yashrtalele@gmail.com>
2024-06-17 18:55:59 +0530

    Added User Auth Middleware for token verification

---
+++ src/middleware/user.ts
@@ -0,0 +1,30 @@
+const JWT_SECRET = process.env.JWT_SECRET;
+import { HTTP_STATUS } from "../constants";
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import { AuthRequest } from "../types";
+
+const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(HTTP_STATUS.UNAUTHORIZED).json({
+      message: "Unauthorized",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET as string) as {
+      userId: string;
+    };
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(HTTP_STATUS.UNAUTHORIZED).json({
+      message: "Unauthorized",
+    });
+  }
+};
